ce1d7891662704125a140f33bc7dfd23f5045294
Nurujjaman Pollob <nurujjamanpollob@gmail.com>
2022-05-14 20:14:06 -0700

    Initial commit

---
+++ src/main/java/com/nurujjamanpollob/fourkpixelbackend/FourKPixelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nurujjamanpollob.fourkpixelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourKPixelBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourKPixelBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nurujjamanpollob/fourkpixelbackend/FourKPixelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurujjamanpollob.fourkpixelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourKPixelBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
